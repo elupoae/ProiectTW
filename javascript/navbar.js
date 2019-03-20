c83f58fd5d8820d4a44e4ae25386a1474a183f6a
Eduard Lupoae <edward_v98@yahoo.com>
2019-03-21 01:57:13 +0200

    Nav-bar
    Am schimbat nav-barul pentru a fi resposiv pe toate dispozitivele.

---
+++ javascript/navbar.js
@@ -0,0 +1,24 @@
+const nav = () => {
+    const navClose = document.querySelector('.nav-close');
+    const nav = document.querySelector('.nav-elements');
+    const navElements = document.querySelectorAll('.nav-elements li');
+    // slide nav-bar
+    navClose.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+
+        // animation elements nav-bar
+        navElements.forEach((item, index) => {
+            if(item.style.animation){
+                item.style.animation = '';
+            } else {
+                item.style.animation = `navFade 0.5s ease forwards ${index / 8+ 0.5}s`;
+            }
+        });
+        // animation close nav-bar
+        navClose.classList.toggle('nav-X');
+    });
+
+
+};
+
+nav();
